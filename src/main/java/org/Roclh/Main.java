eaea1a2d0575f712c20617c1be4e9dd73e5dd103
Bilguun Purevsuren <bilguuk124@gmail.com>
2023-03-22 14:10:02 +0300

    Убрали лишнее

--- src/main/java/org/Roclh/Main.java
+++ src/main/java/org/Roclh/Main.java
@@ -9,7 +9,5 @@ public class Main {
     private static final Logger log = LogManager.getLogger();
     public static void main(String[] args) {
         log.info("Это лаба по тпо, она не должна запускаться, только тестироваться)");
-        int[] array =new int[]{3, 5, 7, 23, 32, 32};
-        System.out.println(Arrays.toString(CountingSort.sort(array)));
     }
 }
